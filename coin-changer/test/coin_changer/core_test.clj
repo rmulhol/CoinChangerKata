21b8f93c9843b96a40ac110198671c0464c6167d
Robert Mulholand <robmulholand@gmail.com>
2015-05-07 14:54:12 -0500

    use clojure.test tests with lein midje autorunner

--- coin-changer/test/coin_changer/core_test.clj
+++ coin-changer/test/coin_changer/core_test.clj
@@ -1,20 +1,20 @@
 (ns coin-changer.core-test
-  (:use midje.sweet)
-  (:use [coin-changer.core]))
+  (:require [clojure.test :refer :all])
+  (:require [coin-changer.core :refer :all])
+  (:require [midje.sweet :refer :all]))
 
-(tabular "about 'change-for'"
-  (fact "makes change for amounts"
-    (change-for ?amount) => ?change)
-  ?amount   ?change
-  0         []
-  1         [1]
-  2         [1 1]
-  3         [1 1 1]
-  5         [5]
-  6         [5 1]
-  10        [10]
-  16        [10 5 1]
-  25        [25]
-  42        [25 10 5 1 1]
-  99        [25 25 25 10 10 1 1 1 1]
-)
+(deftest coin-changer
+  (testing "provides correct change"
+    (are [amount change] (= (change-for amount) change)
+         0  []
+         1  [1]
+         2  [1 1]
+         3  [1 1 1]
+         5  [5]
+         6  [5 1]
+         10 [10]
+         16 [10 5 1]
+         25 [25]
+         42 [25 10 5 1 1]
+         99 [25 25 25 10 10 1 1 1 1]
+         )))
